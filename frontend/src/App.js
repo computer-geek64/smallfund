00419c48ccc2acdeca58a9607c651e475c926901
Sharath Palathingal <palathingal@gatech.edu>
2021-10-23 14:05:49 -0400

    Added axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('')
+
+  const getText = () => {
+    const config = {
+      method: 'get',
+      url: 'http://128.61.105.83:8000/',
+      headers: { }
+    };
+    
+    axios(config)
+    .then(function (response) {
+      setData(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +36,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <button onClick={getText}>Click Me!</button>
+      <br />
+      {data}
     </div>
   );
 }
