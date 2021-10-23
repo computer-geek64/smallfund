caedf9f68c9aa574fe8ad0c36e8e10ecad44592b
Ashish D'Souza <sudouser512@gmail.com>
2021-10-23 17:13:23 -0400

    Added Google Cloud MySQL server

--- backend/server.sh
+++ backend/server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # server.sh
 
+./cloud_sql_proxy -instances=${GCP_INSTANCE_CONNECTION_NAME}=tcp:3306 -credential_file ${GCP_KEY_FILE} > /dev/null 2>&1 &
+python3 /code/src/endpoints/api.py
