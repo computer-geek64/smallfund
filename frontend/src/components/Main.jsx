fbca3b1352464d21498ac4cfb1375ab44fb031f5
Sharath Palathingal <palathingal@gatech.edu>
2021-10-24 04:32:44 -0400

    Finalalized navbar

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,56 +1,16 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
 
 export default function PrimarySearchAppBar() {
   const menuId = 'primary-search-account-menu';
@@ -67,15 +27,25 @@ export default function PrimarySearchAppBar() {
                 HackGT 8
             </Typography>
             <Box sx={{ flexGrow: 1 }} />
-            <Search>
-                <SearchIconWrapper>
-                    <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ 'aria-label': 'search' }}
-                />
-            </Search>
+            <Autocomplete
+                id="combo-box-demo"
+                options={["abc", "bcd"]}
+                getOptionLabel={(option) => option}
+                forcePopupIcon={true}
+                popupIcon={<SearchIcon />}
+                renderInput={(params) => (
+                    <>
+                        <TextField
+                            placeholder="search"
+                            {...params}
+                            label="Search..."
+                            variant="filled"
+                        />
+                    </>
+                )}
+                sx={{ width: 300 }}
+                size="small"
+             />
             <Box sx={{ flexGrow: 1 }} />
             <IconButton
                 size="large"
