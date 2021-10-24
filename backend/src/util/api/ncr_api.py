f30743010e33e8f808fb7dd0ba383e05c53f5a3b
Pranav Pusarla <ppusarla@gmail.com>
2021-10-24 01:09:04 -0400

    addded createConsumer method and changed access token usage

--- backend/src/util/api/ncr_api.py
+++ backend/src/util/api/ncr_api.py
@@ -17,14 +17,13 @@ def calculateSignature():
     signature = base64.b64encode(signatureHash)
     print(signature)
 
-def create_access_token(organization, shared_key, secret_key):
-    regexPattern = / ({{(.* ?)}}) / g;
-
-    signature = calculateSignature()
-    access_key = 'AccessKey ' + shared_key + ':' + signature
+def create_access_token(request_url, request_method):
+    access_token = ""
+    return access_token
 
 def create_item(item_id, item_name, description, price):
     url = "https://gateway-staging.ncrcloud.com/catalog/v2/items/"+item_id
+    request_method = "PUT"
 
     payload = {
         'version': 1,
@@ -65,30 +64,31 @@ def create_item(item_id, item_name, description, price):
 
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'AccessKey 8a4914a9f08b4a45916012763aafe24a:jEwPpu3n23NeYouXHnx8BjUM6AvV2fCvU+7nsbvwnJmA5XZM4OeMIleh/TM1Y388pu1Zw8eZCTpXbaDFIe7bpQ==',
+        'Authorization': create_access_token(url, request_method),
         'nep-organization': 'test-drive-d2525f33ae1741398399d',
         'Date': 'Sat, 23 Oct 2021 21:20:30 GMT',
         'Accept': 'application/json',
         'Accept-Language': 'en-us'
     }
 
-    response = requests.request("PUT", url, headers=headers, data=payload)
+    response = requests.request(request_method, url, headers=headers, data=payload)
     print(response)
 
 def get_item(item_id):
     url = "https://gateway-staging.ncrcloud.com/catalog/v2/items/"+item_id
+    request_method = "GET"
 
     payload = {}
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'AccessKey 8a4914a9f08b4a45916012763aafe24a:mofshVcVG86ew0wO4dZuWI661la5tm6Lam97qd9PpyyrAibEJTch1D38Op0rgdGrxIBdRHFEJSO0wXC6BxJ5oA==',
+        'Authorization': create_access_token(url, request_method),
         'nep-organization': 'test-drive-d2525f33ae1741398399d',
         'Date': 'Sat, 23 Oct 2021 21:20:30 GMT',
         'Accept': 'application/json',
         'Accept-Language': 'en-us'
     }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request(request_method, url, headers=headers, data=payload)
 
     print(response.text)
 
@@ -98,7 +98,43 @@ def get_item(item_id):
 # def delete_item(item_name):
 #
 #
-# def create_seller(name):
+def create_seller(name):
+    url = "https://gateway-staging.ncrcloud.com/cdm/consumers"
+    request_method = "POST"
+
+    payload = {
+        "profileUsername": "bobthebuilder@smallbusiness.com",
+        "firstName": "Bob",
+        "lastName": "the Builder",
+        "effectiveDate": "2021-10-25",
+        "gender": "Male",
+        "phone": "4085671234",
+        "mobile": "4081234567",
+        "addresses": [
+            {
+                "name": "Office",
+                "line1": "864 Spring St NW",
+                "line2": "North Tower. 14th Floor",
+                "city": "Atlanta",
+                "state": "GA",
+                "postalCode": "30308",
+                "country": "USA"
+            }
+        ]
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': create_access_token(url, request_method),
+        'nep-organization': 'test-drive-d2525f33ae1741398399d',
+        'Date': 'Sat, 23 Oct 2021 21:20:30 GMT',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-us'
+    }
+
+    response = requests.request(request_method, url, headers=headers, data=payload)
+
+    print(response.text)
 #
 #
 # def search_items_criteria(name):
