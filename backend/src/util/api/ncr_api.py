380e3719d1384b79ac9eb3173598a87724c71362
Pranav Pusarla <ppusarla@gmail.com>
2021-10-24 06:17:40 -0400

    fixed project

--- backend/src/util/api/ncr_api.py
+++ backend/src/util/api/ncr_api.py
@@ -12,7 +12,6 @@ def send_request(request_url, request_method, payload):
     date = datetime.datetime.now(datetime.timezone.utc)
     date = date.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
-    payload = {}
     headers = {
         'Content-Type': 'application/json',
         'Authorization': create_access_token(request_url, request_method),
@@ -21,12 +20,13 @@ def send_request(request_url, request_method, payload):
         'Accept': 'application/json',
         'Accept-Language': 'en-us'
     }
-
+    if (len(payload) != 0):
+        payload = json.dumps(payload)
     response = requests.request(request_method, request_url, headers=headers, data=payload)
     return response
 
 def create_item(item_id, item_name, description, price, seller_id):
-    url = "https://gateway-staging.ncrcloud.com/catalog/v2/items/"+item_id
+    request_url = "https://gateway-staging.ncrcloud.com/catalog/v2/items/"+item_id
     request_method = "PUT"
 
     payload = {
@@ -64,21 +64,12 @@ def create_item(item_id, item_name, description, price, seller_id):
                 ]
             }
         ]
-
     }
 
-    payload = json.dumps(payload)
-
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': create_access_token(url, request_method),
-        'nep-organization': 'test-drive-d2525f33ae1741398399d',
-        'Date': 'Sat, 23 Oct 2021 21:20:30 GMT',
-        'Accept': 'application/json',
-        'Accept-Language': 'en-us'
-    }
-
-    response = requests.request(request_method, url, headers=headers, data=payload)
+    response = send_request(request_url, request_method, payload)
+    while(response.status_code != 204):
+        response = send_request(request_url, request_method, payload)
+    # print(response)
     return
 
 
@@ -87,8 +78,10 @@ def get_item(item_id):
     request_method = "GET"
 
     response = send_request(request_url, request_method, {})
+    while(response.status_code != 200):
+        response = send_request(request_url, request_method, {})
     data = response.json()
-    print(data)
+    # print(data)
 
     description = data['shortDescription']['values'][0]['value']
     price = data['dynamicAttributes'][0]['attributes'][0]['value']
@@ -100,7 +93,7 @@ def update_item(item_id, item_name, description, price):
     request_url = "https://gateway-staging.ncrcloud.com/catalog/v2/items/" + item_id
     request_method = "GET"
     GET_response = send_request(request_url, request_method, {})
-    response_dict = json.loads(GET_response)
+    response_dict = GET_response.json()
 
     response_dict['version'] += 1
     response_dict['shortDescription']['values'][0]['values'] = description
@@ -116,12 +109,52 @@ def delete_item(item_id):
     request_method = "GET"
 
     GET_response = send_request(request_url, request_method, {})
-    response_dict = json.loads(GET_response)
+    while (GET_response.status_code != 200):
+        GET_response = send_request(request_url, request_method, {})
+    response_dict = GET_response.json()
 
-    response_dict['status'] = 'INACTIVE'
-    request_method = "POST"
-    POST_response = send_request(request_url, request_method, response_dict)
-    print(POST_response.text)
+    payload = {
+        'version': response_dict['version']+1,
+        'shortDescription': {
+            "values": [
+                {
+                    "locale": "en-US",
+                    "value": response_dict['shortDescription']['values'][0]['value']
+                }
+            ]
+        },
+        'merchandiseCategory': {
+            'nodeId': '1-846-188-450'
+        },
+        "status": "INACTIVE",
+        "departmentId": "783497",
+        "nonMerchandise": None,
+        "dynamicAttributes": [
+            {
+                "type": "String",
+                "attributes": [
+                    {
+                        "key": "name",
+                        "value": response_dict['dynamicAttributes'][0]['attributes'][1]['value']
+                    },
+                    {
+                        "key": "price",
+                        "value": response_dict['dynamicAttributes'][0]['attributes'][0]['value']
+                    },
+                    {
+                        "key": "seller",
+                        "value": response_dict['dynamicAttributes'][0]['attributes'][2]['value']
+                    }
+                ]
+            }
+        ]
+
+    }
+
+    request_method = "PUT"
+    PUT_response = send_request(request_url, request_method, payload)
+    while(PUT_response.status_code != 204):
+        PUT_response = send_request(request_url, request_method, payload)
 
 
 def create_seller():
@@ -129,13 +162,13 @@ def create_seller():
     request_method = "POST"
 
     payload = {
-        "profileUsername": "bobthebuilder@smallbusiness.com",
+        'profileUsername': "bobthebuilder33@smallbusiness.com",
         "firstName": "Bob",
         "lastName": "the Builder",
         "effectiveDate": "2021-10-25",
         "gender": "Male",
-        "phone": "4085671234",
-        "mobile": "4081234567",
+        "phone": "4085671266",
+        "mobile": "4081234599",
         "addresses": [
             {
                 "name": "Office",
@@ -150,19 +183,21 @@ def create_seller():
     }
 
     response = send_request(request_url, request_method, payload)
+    while(response.status_code != 200):
+        response = send_request(request_url, request_method, payload)
     data = response.json()
-    print(data)
 
     # put consumer id into database connecting to user
     consumer_id = data['consumerAccountNumber']
     return consumer_id
 
-def get_customer(consumer_account_number):
+def get_seller(consumer_account_number):
     request_url = "https://gateway-staging.ncrcloud.com/cdm/consumers"+consumer_account_number
     request_method = "GET"
 
     response = send_request(request_url, request_method, {})
     data = response.json()
+    print(data)
 
     username = data['profileUsername']
     first_name = data['firstName']
@@ -178,9 +213,9 @@ def list_of_objects():
     request_method = "GET"
 
     response = send_request(request_url, request_method, {})
+    while(response.status_code != 200):
+        response = send_request(request_url, request_method, {})
     data = response.json()['pageContent']
-    print(type(data))
-    print(data)
 
     all_objects = []
 
@@ -190,7 +225,10 @@ def list_of_objects():
         description = data[i]["shortDescription"]["value"]
 
         response = send_request("https://gateway-staging.ncrcloud.com/catalog/v2/items/"+item_id, "GET", {})
+        while(response.status_code != 200):
+            response = send_request("https://gateway-staging.ncrcloud.com/catalog/v2/items/" + item_id, "GET", {})
         response_data = response.json()
+        # print(response_data)
         price = response_data['dynamicAttributes'][0]['attributes'][0]['value']
         name = response_data['dynamicAttributes'][0]["attributes"][1]['value']
         item['id'] = item_id
@@ -204,5 +242,21 @@ def list_of_objects():
 
 # def search_items_criteria(name):
 
-print(create_access_token("https://gateway-staging.ncrcloud.com/catalog/v2/items/itemObject", 'GET'))
-print(list_of_objects())
+# print(create_access_token("https://gateway-staging.ncrcloud.com/catalog/v2/items/itemObject", 'GET'))
+# consumer_id = create_seller()
+# print("Seller created with id {}".format(consumer_id))
+# username, first_name, birthday, last_name, phone, mobile = get_seller("SAIA1XKMZ9R3IGXU")
+# print("Seller {} {} retrieved".format(first_name, last_name))
+# item_id = "11"
+# create_item(item_id, "Ashish", "dumb but", "1", consumer_id)
+# print("Item Ashish with id {} created with price 1".format(item_id))
+# sleep(5)
+# name, description, price = get_item(item_id)
+# print("Item {} described as {} and price {} retrieved".format(name, description, price))
+# print("All objects: \n")
+# print(list_of_objects())
+# delete_item(item_id)
+# print("Item {} deleted".format(item_id))
+# sleep(5)
+# print("Now all objects are: \n")
+# print(list_of_objects())
