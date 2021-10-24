0f45f53eba38760da2b954ea370b85c7525b69dd
Sharath Palathingal <palathingal@gatech.edu>
2021-10-24 07:22:31 -0400

    Added image hover

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,16 @@
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+
 const Card = (props) => {
     return (
         <div className="card-container">
-            <a className="item-img" href={"/" + props.itemId}><img className="item-img" src={props.img} alt={props.itemName}/></a>
+            <div className="spl-container">
+                <img className="item-img" src={props.img} alt={props.itemName}/>
+                <a href={"/product/" + props.itemId}>
+                    <div class="overlay">
+                        <ControlPointIcon className="icon"/>
+                    </div>
+                </a>
+            </div>
             <span className="item-name">{props.itemName}</span>
             <span className="item-price">${props.price}</span>
         </div>
