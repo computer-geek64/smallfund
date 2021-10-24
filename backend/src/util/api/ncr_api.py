f68ef82c04e87170f4f3ba0f33e88723c42ebcbd
Varun Lakshmanan <varunlakshmanan@outlook.com>
2021-10-24 01:32:21 -0400

    Added update item API

--- backend/src/util/api/ncr_api.py
+++ backend/src/util/api/ncr_api.py
@@ -91,9 +91,29 @@ def get_item(item_id):
 
     print(response.text)
 
-# def update_item(item_name):
-#
-#
+def update_item(item_id, item_name, description, price):
+    url = "https://gateway-staging.ncrcloud.com/catalog/v2/items/" + item_id
+    payload = {}
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'AccessKey 8a4914a9f08b4a45916012763aafe24a:mofshVcVG86ew0wO4dZuWI661la5tm6Lam97qd9PpyyrAibEJTch1D38Op0rgdGrxIBdRHFEJSO0wXC6BxJ5oA==',
+        'nep-organization': 'test-drive-d2525f33ae1741398399d',
+        'Date': 'Sat, 23 Oct 2021 21:20:30 GMT',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-us'
+    }
+    GET_response = requests.request("GET", url, headers=headers, data=payload)
+    response_dict = json.loads(GET_response)
+
+    response_dict['version'] += 1
+    response_dict['shortDescription']['values'][0]['values'] = description
+    response_dict['dynamicAttributes'][0]['attributes'][0]['value'] = price
+    response_dict['dynamicAttributes'][0]['attributes'][1]['value'] = item_name
+    POST_response = requests.request("PUT", url, headers=headers, data=response_dict)
+    print(POST_response.text)
+
+
+
 # def delete_item(item_name):
 #
 #
