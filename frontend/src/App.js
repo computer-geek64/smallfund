f816fecd5a37b453131f31fbf5883d7d947ba798
Sharath Palathingal <palathingal@gatech.edu>
2021-10-24 04:48:56 -0400

    Added react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
-import Main from './components/Main'
+import Navbar from './components/Navbar'
 import NewListing  from './components/NewListing';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
 
 const darkTheme = createTheme({
@@ -13,8 +19,15 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <div className="App">
-        <Main/>
-        <NewListing/>
+        <Router>
+          <Navbar/>
+          
+          <Switch>
+            <Route path="/add">
+              <NewListing/>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </ThemeProvider>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import { useState } from 'react';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -12,8 +13,12 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
-export default function PrimarySearchAppBar() {
+import { Link } from "react-router-dom";
+
+export default function Navabr() {
   const menuId = 'primary-search-account-menu';
+  const [auth, setAuth] = useState(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -54,29 +59,35 @@ export default function PrimarySearchAppBar() {
                 aria-controls={menuId}
                 aria-haspopup="true"
                 color="inherit"
+                component={Link}
+                to="/add"
             >
                 <ControlPointIcon />
             </IconButton>
-            <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                color="inherit"
-            >
-                <AccountCircle />
-            </IconButton>
-            <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                color="inherit"
-            >
-                <ExitToAppIcon />
-            </IconButton>
+            { auth && 
+                <IconButton
+                    size="large"
+                    edge="end"
+                    aria-label="account of current user"
+                    aria-controls={menuId}
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+            }
+            { auth && 
+                <IconButton
+                    size="large"
+                    edge="end"
+                    aria-label="account of current user"
+                    aria-controls={menuId}
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <ExitToAppIcon />
+                </IconButton>
+            }
         </Toolbar>
       </AppBar>
     </Box>
